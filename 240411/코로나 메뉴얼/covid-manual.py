e25c2c19368c7c1265048426962399d61df1610c
sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2024-04-11 02:56:22 +0900

    Title: 코로나 메뉴얼, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/코로나 메뉴얼/covid-manual.py
+++ 240411/코로나 메뉴얼/covid-manual.py
@@ -13,13 +13,13 @@ jarr=j.split()
 jcold=jarr[0]
 jtem = int(jarr[1])
 
-A=0
-B=0
-C=0
-D=0
-
 if icold=='Y' and item>=37:
     if (kcold=='Y' and ktem>=37) or (jcold=='Y' and jtem>=37):
+        print('E')
+    else:
+        print('N')
+else:
+    if (kcold=='Y' and ktem>=37) and (jcold=='Y' and jtem>=37):
         print('E')
     else:
         print('N')
