69bde50ae7fdf5a7232ff1a1d14a154aa54c68ec
sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2024-04-11 00:45:51 +0900

    Title: 4가지 관계연산자, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240411/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,25 @@
+i=input()
+arr=i.split()
+
+a=int(arr[0])
+b=int(arr[1])
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a<=b:
+    print(1)
+else:
+    print(0)
+
+if a<b:
+    print(1)
+else:
+    print(0)
