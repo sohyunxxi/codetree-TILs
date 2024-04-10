545dc88b98961836df0052bcc605aeed942079aa
sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2024-04-11 00:50:47 +0900

    Title: 점수 비교, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/점수 비교/score-comparison.py
+++ 240411/점수 비교/score-comparison.py
@@ -1,17 +1,15 @@
-i=input()
+inp = input()
+arr = inp.split()
+a_math = int(arr[0])
+a_eng = int(arr[1])
 
-arr=i.split()
+inp = input()
+arr = inp.split()
+b_math = int(arr[0])
+b_eng = int(arr[1])
 
-a=int(arr[0])
-b=int(arr[1])
-
-t=input()
-krr=t.split()
-
-c=int(krr[0])
-d=int(krr[1])
-
-if a>b and c>d:
-    print(1)
+# 출력
+if a_math > b_math and a_eng > b_eng:
+	print(1)
 else:
-    print(0)
+	print(0)
