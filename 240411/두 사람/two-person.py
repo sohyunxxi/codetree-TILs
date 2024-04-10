23eac16ec1231c1e6523e5b877671c16dfde181e
sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2024-04-11 02:25:03 +0900

    Title: 두 사람, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/두 사람/two-person.py
+++ 240411/두 사람/two-person.py
@@ -12,10 +12,9 @@ brr=b.split()
 bAge=int(brr[0])
 bSex=brr[1]
 
-if aAge>=19 or bAge>=19:
-    if aSex=="M" or bSex=="M":
-        print(1)
-    else:
-        print(0)
+if aAge>=19 and aSex=='M':
+    print(1)
+elif bAge>=19 and bSex=='M':
+    print(1)
 else:
     print(0)
