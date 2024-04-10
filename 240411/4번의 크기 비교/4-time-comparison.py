5fd31412eaf422645435e8548895293e8a6908f2
sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2024-04-11 00:48:15 +0900

    Title: 4번의 크기 비교, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/4번의 크기 비교/4-time-comparison.py
+++ 240411/4번의 크기 비교/4-time-comparison.py
@@ -9,20 +9,20 @@ e=int(arr[3])
 
 if a>b:
     print(1)
-else
+else:
     print(0)
 
 if a>c:
     print(1)
-else
+else:
     print(0)
 
 if a>d:
     print(1)
-else
+else:
     print(0)
 
 if a>e:
     print(1)
-else
+else:
     print(0)
